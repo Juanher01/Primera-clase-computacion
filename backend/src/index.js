1508b0c8fe0967314cf0e0de29f3db55394bd090
Juanher01 <u20222209033@usco.edu.co>
2025-09-02 13:51:00 -0500

    feat: conexion y pruebas con post

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import morgan from 'morgan'
+import { connectDB } from './db.js';
+
+import authRoutes from './routes/auth.routes.js'
 
 
 const app = express();
-
 app.use(morgan('dev'))
+connectDB()
+app.use(authRoutes)
 
 app.listen(4500)
 console.log('SERVIDOR CORRIENDO', 4500);
